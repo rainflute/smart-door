e0bd8a4e0e53cc7819e8f51094c7166389251af7
Yuxiao Tan <ytan@Yuxiaos-MacBook-Pro.local>
2017-05-04 22:44:11 -0400

    refactored

--- doorListener.py
+++ doorListener.py
@@ -1,6 +1,39 @@
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
-from shadowCallBackContainer import ShadowCallBackContainer
 import os
+import json
+import door
+
+
+class ShadowCallbackContainer:
+    def __init__(self, client):
+        self.client = client
+
+    def call_back_delta(self, payload, responseStatus, token):
+        payloadDict = json.loads(payload)
+        deltaMessage = json.dumps(payloadDict["state"])
+        deltaMessage = self.handle(deltaMessage)
+        newPayload = '{"state":{"reported":' + deltaMessage + '}}'
+        self.client.shadowUpdate(newPayload, None, 5)
+
+    def handle(self, deltaMessage):
+        try:
+            if deltaMessage == 'open':
+                door.open()
+            elif deltaMessage == 'lock':
+                door.lock()
+            return deltaMessage
+        except RuntimeError:
+            if deltaMessage == 'open':
+                return 'lock'
+            elif deltaMessage == 'lock':
+                return 'open'
+
+    def initializeDoor(self, payload):
+        payloadDict = json.loads(payload)
+        status = json.dumps(payloadDict["state"]['reported']['status'])
+        status = self.handle(status)
+        newPayload = '{"state":{"reported":' + status + '}}'
+        self.client.shadowUpdate(newPayload, None, 5)
 
 host = "a108by5cx6oj8b.iot.us-west-2.amazonaws.com"
 keyPath = os.path.dirname(os.path.abspath(__file__)) + '/../keys'
@@ -23,7 +56,7 @@ myAWSIoTMQTTShadowClient.connect()
 # Create a deviceShadow with persistent subscription
 Bot = myAWSIoTMQTTShadowClient.createShadowHandlerWithName("door", True)
 
-callbackContainer = ShadowCallBackContainer.ShadowCallbackContainer(Bot)
+callbackContainer = ShadowCallbackContainer(Bot)
 # Initialize door
 Bot.shadowGet(callbackContainer.initializeDoor)
 # Listen on deltas
